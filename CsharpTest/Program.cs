1287693ab047c12b27e275daa113b3e83a716d7e
Stefano Laptop <stefanohu926@gmail.com>
2024-03-07 12:08:52 +0100

    c sharp test

---
+++ CsharpTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CsharpTest
+{
+    public class Program(string[] args)
+    {
+        public static void Main(string[] args)
+        {
+            l s = new l();
+            s.hello();
+        }
+
+        struct l
+        {
+            public void hello()
+            {
+                Console.WriteLine("Hello world");
+            }
+        }
+
+
+    }
+}
