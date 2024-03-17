ed29af451c91c2c5dba5766eef57d9acefe5cc08
Stefano Laptop <stefanohu926@gmail.com>
2024-03-17 19:39:40 +0100

    semantic (non tested)

--- JavaFunctionalCpp/semantic.cpp
+++ JavaFunctionalCpp/semantic.cpp
@@ -1,5 +1,14 @@
 #include "semantic.hpp"
 #include "binaryexpression.hpp"
+#include "boolnode.hpp"
+#include "identifiernode.hpp"
+#include "numbernode.hpp"
+#include "stringnode.hpp"
+#include "unarynode.hpp"
+#include "expressionstmtnode.hpp"
+#include "printstmtnode.hpp"
+#include "varassignmentstmtnode.hpp"
+#include "vardeclarationnode.hpp"
 
 Semantic::Semantic(Enviroment env)
 {
@@ -43,6 +52,96 @@ std::any Semantic::visitBinaryExpression(BinaryExpression& binaryExpression)
 			break;
 
 	}
+
+	return std::any();
+}
+
+std::any Semantic::visitBoolNode(BoolNode& boolNode)
+{
+	bool v = boolNode.value;
+	if (not (v == false || v == true)) 
+	{
+		add_err("Boolean value is invalid (must be either 'true' or 'false').");
+	}
+	return std::any();
+}
+
+std::any Semantic::visitNumberNode(NumberNode& numberNode)
+{
+	if (not isdigit(numberNode.number))
+	{
+		add_err("Number value is invalid.");
+	}
+
+	return std::any();
+}
+
+std::any Semantic::visitStringNode(StringNode& stringNode)
+{
+	return std::any();
+}
+
+std::any Semantic::visitIdentifierNode(IdentifierNode& identifierNode)
+{
+	try 
+	{
+		this->env.get(identifierNode.identifier)
+	}
+	catch (std::invalid_argument e)
+	{
+		add_err(e.what());
+	}
+	return std::any();
+}
+
+std::any Semantic::visitUnaryNode(UnaryNode& unaryNode)
+{
+	unaryNode.left->accept(*this);
+	return std::any();
+}
+
+std::any Semantic::visitExpressionStmt(ExpressionStmtNode& expressionStmtNode)
+{
+	expressionStmtNode.expression->accept(*this);
+	return std::any();
+}
+
+std::any Semantic::visitPrintStmt(PrintStmtNode& printStmtNode)
+{
+	printStmtNode.expression->accept(*this);
+	return std::any();
+}
+
+std::any Semantic::visitVarDeclarationStmt(VarDeclarationNode& varDeclarationNode)
+{
+	Variable var;
+	var.identifier = varDeclarationNode.identifier;
+	var.dtType = tokent2datatype(varDeclarationNode.variableType);
+	varDeclarationNode.expression->accept(*this);
+	try
+	{
+		this->env.set(var);
+	}
+	catch (std::invalid_argument e)
+	{
+		add_err(e.what());
+	}
+	return std::any();
+}
+
+std::any Semantic::visitVarAssignmentStmt(VarAssignmentStmtNode& varAssignmentNode)
+{
+	varAssignmentNode.expression->accept(*this);
+	try
+	{
+		this->env.get(varAssignmentNode.identifier);
+		// check value and var data type
+	}
+	catch (std::invalid_argument e)
+	{
+		add_err(e.what());
+	}
+	return std::any();
 }
 
 void Semantic::add_err(std::string error)
